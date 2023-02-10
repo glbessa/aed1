e748db8e489a0214918f2cf3ac0e9bdc9ad94e8b
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2023-02-10 14:16:07 -0300

    Formatando o código para ficar mais legível

--- tarefa1/romanToInteger.c
+++ tarefa1/romanToInteger.c
@@ -1,4 +1,13 @@
-// Problema Roman to Integer do LeetCode
+/**********************************************************************************
+ * Problema Roman to Integer do LeetCode                                          *
+ *                                                                                *
+ * Fazer um programa que dado um certo número romano converta ele para um inteiro * 
+ * https://leetcode.com/problems/roman-to-integer/description/                    *
+ *                                                                                *
+ *                                                                                *
+ **********************************************************************************/
+
+// 
 // https://leetcode.com/problems/roman-to-integer/description/
 
 #include <stdio.h>
@@ -13,23 +22,25 @@ int main()
 
 	int number = 0;
 	int i = 0; //contador de posicoes
+
+	/*
+	 *  Rotina para verificar os algarismos romanos e converte-los para inteiro
+	 */
+	
 	while (romanNumber[i] != '\0')
 	{
 		switch (romanNumber[i])
 		{
 			case 'I':
-				if (romanNumber[i+1] == 'V') //verificando os casos IV
-				{
+				if (romanNumber[i+1] == 'V') {
+					/* verificando os casos IV */
 					number += 4;
 					i++;
-				}
-				else if (romanNumber[i+1] == 'X') //verificando os caso IX
-				{
+				} else if (romanNumber[i+1] == 'X') {
+					/* verificando os caso IX */
 					number += 9;
 					i++;
-				}
-				else 
-				{
+				} else {
 					number += 1;
 				}
 				break;
@@ -37,18 +48,15 @@ int main()
 				number += 5;
 				break;
 			case 'X':
-				if (romanNumber[i+1] == 'L') //verificando os casos XL
-				{
+				if (romanNumber[i+1] == 'L') { 
+					/* verificando os casos XL */
 					number += 40;
 					i++;
-				}
-				else if (romanNumber[i+1] == 'C') //verificando os casos XC
-				{
+				} else if (romanNumber[i+1] == 'C') {
+					/* verificando os casos XC */
 					number += 90;
 					i++;
-				}
-				else 
-				{
+				} else {
 					number += 10;
 				}
 				break;
@@ -56,18 +64,15 @@ int main()
 				number += 50;
 				break;
 			case 'C':
-				if (romanNumber[i+1] == 'D') //verificando os casos CD
-				{
+				if (romanNumber[i+1] == 'D') {
+					/* verificando os casos CD */
 					number += 400;
 					i++;
-				}
-				else if (romanNumber[i+1] == 'M') //verificando os casos CM
-				{
+				} else if (romanNumber[i+1] == 'M')	{
+					/* verificando os casos CM */
 					number += 900;
 					i++;
-				}
-				else 
-				{
+				} else {
 					number += 100;
 				}
 				break;
